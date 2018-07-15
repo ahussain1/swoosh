a4596fa67ec50e0c5afc56f6a2c552b6c3a5c2ef
Arif Hussain <hussain.arif33@gmail.com>
2018-07-15 19:54:21 -0400

    Segues

---
+++ Controller/LeagueVC.swift
@@ -0,0 +1,34 @@
+//
+//  LeagueVS.swift
+//  swoosh
+//
+//  Created by Arif - Developer  on 7/15/18.
+//  Copyright © 2018 Arif. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
