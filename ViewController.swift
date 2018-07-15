ed35ff04e4865fd0061100e94ad489fa70e19f02
Arif Hussain <hussain.arif33@gmail.com>
2018-07-15 19:23:27 -0400

    Segues and unwind

--- ViewController.swift
+++ ViewController.swift
@@ -7,17 +7,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        
-        bgImg.frame = view.frame
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
         
     }
+    
 
 
 }
